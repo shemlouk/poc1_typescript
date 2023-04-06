9ac13df043fd248a091e38409d43405463831580
samuel <shemlouk@gmail.com>
2023-04-06 18:28:23 -0300

    feat(schemas): add tasks schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -4,3 +4,10 @@ export const roommate = z.object({
   email: z.string().email(),
   name: z.string(),
 });
+
+export const tasks = z.object({
+  title: z.string(),
+  description: z.string(),
+  date: z.date().min(new Date()),
+  roommateId: z.number().int().positive(),
+});
