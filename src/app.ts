a8630bd8e1649801fc196b2fd06d8935f31c82cd
samuel <shemlouk@gmail.com>
2023-04-06 17:43:18 -0300

    feat: add routers and errors middleware to app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
+import errorHandler from "middlewares/errorHandler";
 import express, { json } from "express";
+import router from "routers";
 import cors from "cors";
 
 const app = express();
 
 app.use(json());
 app.use(cors());
+app.use(router);
+app.use(errorHandler);
 
 app.listen(5000, () => {
   console.log("Server is running.");
