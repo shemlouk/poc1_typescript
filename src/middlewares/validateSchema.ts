93ce0b3ebdada6df2206568d418290f083070e33
samuel <shemlouk@gmail.com>
2023-04-06 18:09:55 -0300

    feat(middlewares): add custom error

--- src/middlewares/validateSchema.ts
+++ src/middlewares/validateSchema.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { ZodObject } from "zod";
+import errors from "errors";
 
 const validateSchema = (schema: ZodObject<any>) => {
   return (req: Request, res: Response, next: NextFunction) => {
@@ -7,8 +8,10 @@ const validateSchema = (schema: ZodObject<any>) => {
       schema.parse(req.body);
       next();
     } catch (error) {
-      error.statusCode = 422;
-      next(error);
+      const message = error.issues
+        .map((m: any) => `${m.path[0]} - ${m.message.toLowerCase()}`)
+        .join(", ");
+      next(errors.invalidBody(message));
     }
   };
 };
