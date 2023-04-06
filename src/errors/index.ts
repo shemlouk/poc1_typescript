11a015d887d2c089c9c98628533b18cffd624e04
samuel <shemlouk@gmail.com>
2023-04-06 17:28:25 -0300

    refactor(errors): remove function type

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,24 +1,18 @@
 import ErrorObj from "protocols/ErrorObj";
 
-type ErrorFunction<T> = (params?: T) => ErrorObj;
-
-const createError: ErrorFunction<Partial<ErrorObj>> = ({
-  name,
-  message,
-  statusCode,
-}) => {
+const createError = (name: string, message: string, statusCode: number) => {
   const newError: ErrorObj = new Error(message || "An error has ocurred.");
   newError.name = name || "UnknownError";
   newError.statusCode = statusCode || 500;
   return newError;
 };
 
-const conflictError: ErrorFunction<string> = (item) => {
-  return createError({
-    name: "ConflictError",
-    message: `${item} already exists in database.`,
-    statusCode: 409,
-  });
+const conflictError = (item: string) => {
+  return createError(
+    "ConflictError",
+    `${item} already exists in database.`,
+    409
+  );
 };
 
 export default { createError, conflictError };
