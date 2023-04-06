9cd3e2603ba39624a5438b2ca87e09579e3efb37
samuel <shemlouk@gmail.com>
2023-04-06 18:15:42 -0300

    feat(routers): add tasks router

---
+++ src/routers/TasksRouter.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const tasksRouter = Router();
+
+tasksRouter.post("/");
+
+export default tasksRouter;
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,8 +1,10 @@
 import roommateRouter from "./RoommatesRouter";
+import tasksRouter from "./TasksRouter";
 import { Router } from "express";
 
 const router = Router();
 
-router.use(`/roommates`, roommateRouter);
+router.use("/roommates", roommateRouter);
+router.use("/tasks", tasksRouter);
 
 export default router;
