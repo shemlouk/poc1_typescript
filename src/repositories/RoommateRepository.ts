13bf839d29b3b0bdfb8cb46f287039cfdb88f567
samuel <shemlouk@gmail.com>
2023-04-06 21:55:26 -0300

    feat(repositories): add getById to roommate repository

--- src/repositories/RoommateRepository.ts
+++ src/repositories/RoommateRepository.ts
@@ -1,12 +1,12 @@
 import { Roommate } from "@prisma/client";
 import prisma from "../config/database";
 
-const create = async (data: Omit<Roommate, "id">): Promise<Roommate> => {
+const create = async (data: Omit<Roommate, "id">) => {
   const res = await prisma.roommate.create({ data });
   return res;
 };
 
-const findByEmail = async (email: string): Promise<Roommate> => {
+const findByEmail = async (email: string) => {
   const res = await prisma.roommate.findUnique({
     where: {
       email,
@@ -15,4 +15,9 @@ const findByEmail = async (email: string): Promise<Roommate> => {
   return res;
 };
 
-export default { create, findByEmail };
+const getById = async (id: number) => {
+  const res = await prisma.roommate.findUnique({ where: { id } });
+  return res;
+};
+
+export default { create, findByEmail, getById };
